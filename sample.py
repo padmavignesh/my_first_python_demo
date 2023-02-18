ee3354d94dc5068f06d9766840feb73caef8b038
padmavignesh <119202677+padmavignesh@users.noreply.github.com>
2023-02-18 22:41:41 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,8 @@
+print("Hi,am Padma")
+
+def my_function():
+  print("This is demo for python using superlinter"
+        
+        
+ print("This is my first demo github using python file")
+ my_function();
