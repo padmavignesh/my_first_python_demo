7e0693f9c173e18ef1a2448aa910e2d4266b4959
padmavignesh <119202677+padmavignesh@users.noreply.github.com>
2023-02-18 22:57:06 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,7 +2,7 @@ print("Hi,am Padma")
 
 
 def my_function():
-  print("This is demo for python using superlinter"
+    print("This is demo for python using superlinter"
         
         
  print("This is my first demo github using python file")
